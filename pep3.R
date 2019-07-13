cc27787c33811d503b39aa7e357b192a37c0a48d
Unknown <jorge.plaza@usach.cl>
2019-07-13 12:20:51 -0400

    Comentarios

--- pep3.R
+++ pep3.R
@@ -1,6 +1,22 @@
 # PEP 3 Inferencia y Modelos Estadisticos
 # Profesor: Jose Luis Jara
 
+# Los datos fueron recolectados en terreno por los estudiantes
+# que realizan el analisis. 
+# Se basa en una encuesta a las personas que asistieron a ver 
+# el eclipse total sol de la Region de Coquimbo en Chile durante
+# el 2 de Julio de 2019, se estudian las siguientes variables:
+
+# Procedencia: Regional, Metropolitana o Extranjera
+# Continente: El continente de donde viene 
+# Origen: Lugar de origen (Pais, Ciudad, Region)
+# Nacionalidad: La nacionalidad del encuestado
+# Edad
+# Noches: Noches de estadia en la region
+# Sexo
+# Transporte: Transporte que utiliza para llegar a la region
+# Hospedaje: 
+
 
 library(ggpubr)
 library(ggplot2)
@@ -20,10 +36,6 @@ file <- file.path(dir, basename)
 datos.todos <- read.csv (
   file = file,
   sep = ","
-)
-
-datos.long <- gather(
-  data = datos.todos
 )
 
 hist(datos.todos$Edad)
