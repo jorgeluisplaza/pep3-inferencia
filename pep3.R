0021382bc729586bc196999c61a25ba90832cf21
Unknown <jorge.plaza@usach.cl>
2019-07-13 12:18:21 -0400

    Comentarios varios

--- pep3.R
+++ pep3.R
@@ -1,8 +1,11 @@
 # PEP 3 Inferencia y Modelos Estadisticos
 # Profesor: Jose Luis Jara
 
+
 library(ggpubr)
 library(ggplot2)
+library(knitr)
+library(tidyr)
 
 # Indicar directorio del archivo .csv
 dir <- "/Users/jorgeplaza/Documents/Usach/Inferencia/Pep3"
@@ -19,7 +22,24 @@ datos.todos <- read.csv (
   sep = ","
 )
 
+datos.long <- gather(
+  data = datos.todos
+)
+
 hist(datos.todos$Edad)
 
 hist(datos.todos$Presupuesto)
+
+hist(datos.todos$Noches)
+
+# Dias de nacimiento
+
+# Matias Paredes: d1 = 10
+# Jorge Plaza: d2 = 13
+# Vicente Rivera: d3 = 10
+# Nicolas Alarcon: d4 = 6
+
+# Semilla: d1 * d2 + d3 * d4 = 10 * 13 + 10 * 6 = 190
+
+
 
