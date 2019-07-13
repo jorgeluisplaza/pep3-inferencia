6ec409dae13304d9483744238c2582fc1555b0aa
asdfghjkln1 <vicente.rivera.r@usach.cl>
2019-07-13 14:33:30 -0500

    pregunta 1

--- pep3.R
+++ pep3.R
@@ -21,28 +21,70 @@
 library(ggpubr)
 library(ggplot2)
 library(knitr)
+library(boot)
 library(tidyr)
+library(devtools)
+#install_github("DFJL/SamplingUtil")
+library(SamplingUtil)
 
 # Indicar directorio del archivo .csv
-dir <- "/Users/jorgeplaza/Documents/Usach/Inferencia/Pep3"
+dir <- ""
 
 # Indicar nombre del archivo
 basename <- "eclipse2019.csv"
 
 # Se lee el archivo   
-file <- file.path(dir, basename)
+file <- file.path(basename)
 
 # Se guardan los datos en una tabla
 datos.todos <- read.csv (
   file = file,
-  sep = ","
+  sep = ",",
+  encoding = "UTF-8"
 )
+#hist(datos.todos$Edad)
+#hist(datos.todos$Presupuesto)
+#hist(datos.todos$Noches)
+set.seed(190)
+tabla <- data.frame(Procedencia=datos.todos$Procedencia,Localidad=datos.todos$Localidad)
+n.sys <- 30
+index <- sys.sample(N=nrow(tabla), n=n.sys)
+muestra <- tabla[c(index), ]
+frec <- 1:nrow(muestra)
+p1.1 <- aggregate(frec ~ Procedencia + Localidad, data = muestra, FUN = length, drop=FALSE)
+vicuña <- p1.1[c(which(p1.1$Localidad == "Vicuña")), "frec"]
+higuera <- p1.1[c(which(p1.1$Localidad == "La Higuera")), "frec"]
+serena <- p1.1[c(which(p1.1$Localidad == "La Serena")), "frec"]
+table.p1.1 <- data.frame(Vicuña=vicuña, "La Higuera"=higuera, "La Serena"=serena)
+rownames(table.p1.1) <- c("Chileno", "Extranjero")
 
-hist(datos.todos$Edad)
 
-hist(datos.todos$Presupuesto)
+foo <- function(data, indices){
+  frec <- 1:nrow(data)
+  data <-  data[c(indices), ]
+  p1.1 <- aggregate(frec ~ Procedencia + Localidad, data = data, FUN = length, drop=FALSE)
+  vicuña <- p1.1[c(which(p1.1$Localidad == "Vicuña")), "frec"]
+  higuera <- p1.1[c(which(p1.1$Localidad == "La Higuera")), "frec"]
+  serena <- p1.1[c(which(p1.1$Localidad == "La Serena")), "frec"]
+  table.p1.1 <- data.frame(Vicuña=vicuña, "La Higuera"=higuera, "La Serena"=serena)
+  rownames(table.p1.1) <- c("Chileno", "Extranjero")
+  chi <- chisq.test(table.p1.1)
+  return(chi$statistic)
+}
 
-hist(datos.todos$Noches)
+n.perm <- 5000
+bootobj <- boot(muestra, foo, R = n.perm)
+distribucion <- bootobj$t
+alpha <- 0.05
+observado <- chisq.test(table.p1.1)$statistic
+count <- sum(distribucion > observado)
+p.value <- (count + 1)/(n.perm + 1)
+p.value
+p.95 <- alpha*n.perm
+limit <- distribucion[p.95]
+hist(distribucion)
+abline(v=observado, col="blue")
+abline(v=limit, col="red")
 
 # Dias de nacimiento
 
